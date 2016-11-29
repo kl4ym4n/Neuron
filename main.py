30d04b8f250df52aea8b3118adc412443718f7c5
Klayman <kl4ym4n@gmail.com>
2016-11-29 14:12:20 +0700

    finished OR Perceptron

--- main.py
+++ main.py
@@ -1,14 +1,58 @@
+from numpy import *
+
 class Perceptron():
-    def fire(self, nData, N, M, activation, weights, inputs):
-        for data in range(nData):
-            for n in range(N):
-                activation[data][0] = 0
-                for m in range(M+1):
-                    activation[data][n] = weights[m][n] * inputs[data][m]
 
-                if activation[data][n] > 0:
-                    activation[data][n] = 1
-                else:
-                    activation[data][n] = 0
+    def __init__(self, inputs, targets):
 
-        return activation
+        # ndim - number of array dimensions
+        if ndim(inputs) > 1:
+            # shape - tuple of array dimensions
+            self.nIn = shape(inputs)[1]
+        else:
+            self.nIn = 1
+
+        if ndim(targets) > 1:
+            self.nOut = shape(targets)[1]
+        else:
+            self.nOut = 1
+
+        self.nData = shape(inputs)[0]
+
+        # Initialise network
+        self.weights = random.rand(self.nIn + 1, self.nOut) * 0.1 - 0.05  # set weights [0, 1) * 0.1 - 0.05
+
+    def pcntrain(self, inputs, targets, eta, nIterations):
+        # Add the inputs that match the bias node
+        inputs = concatenate((inputs, -ones((self.nData, 1))), axis=1)
+
+        # Training
+        change = arange(self.nData)
+        for n in range(nIterations):
+            self.outputs = self.pcnfwd(inputs)
+            self.weights += eta * dot(transpose(inputs), targets - self.outputs)
+
+            print("Iteration: ", n)
+            print(self.weights)
+
+        activations = self.pcnfwd(inputs)
+        print("Final outputs are:")
+        print(activations)
+
+            # Randomise order of inputs
+            # random.shuffle(change)
+            # inputs = inputs[change, :]
+            # targets = targets[change, :]
+            # return self.weights
+
+    def pcnfwd(self, inputs):
+        outputs = dot(inputs, self.weights)
+        # Threshold the outputs
+        return where(outputs > 0, 1, 0)
+
+
+inputs = array([[0, 0], [0, 1], [1, 0], [1, 1]])
+targets = array([[0], [1], [1], [1]])
+eta = 0.25
+nIterations = 10
+pcp = Perceptron(inputs, targets)
+pcp.pcntrain(inputs, targets, eta, nIterations)
